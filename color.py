ec8cc18219ca8f89457b8a5e3a412817585bcf25
Akhilaa22 <120076482+Akhilaa22@users.noreply.github.com>
2023-01-31 13:01:13 +0530

    Add files via upload

---
+++ color.py
@@ -0,0 +1,4 @@
+import random
+day=['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+colour=['red','blue','yellow','green','purple','pink','black']
+print(random.choice(day)+random.choice(colour))
